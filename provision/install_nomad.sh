858379a9ad108510a2cd7b6e3e3f73181d68543b
Paul Preuvost <paulantoinepreuvostpro@gmail.com>
2024-08-12 13:54:23 +0200

    Edt Provision && Add Provision test verification

--- provision/install_nomad.sh
+++ provision/install_nomad.sh
@@ -3,4 +3,7 @@ echo "START INSTALL NOMAD"
 curl -L -o nomad.zip "https://releases.hashicorp.com/nomad/1.8.2/nomad_1.8.2_linux_amd64.zip"
 sudo unzip nomad.zip -d /usr/local/bin
 sudo chmod +x /usr/local/bin/nomad
+
 nomad --version
+
+echo "NOMAD INSTALLATION COMPLETED"
--- provision/install_terraform.sh
+++ provision/install_terraform.sh
@@ -1,12 +1,11 @@
 echo "START INSTALL TERRAFORM"
 
-# Télécharger et ajouter la clé GPG HashiCorp
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-# Ajouter le dépôt HashiCorp à la liste des sources
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-# Mettre à jour la liste des paquets et installer Terraform sans prompts
 sudo apt-get update -y && sudo apt-get install -y terraform
 
-echo "FINISHED INSTALL TERRAFORM"
+terraform --version
+
+echo "TERRAFORM INSTALLATION COMPLETED"
