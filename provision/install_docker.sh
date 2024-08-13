efd16dd12f797680e95a5ccf192fd9e7a9d32019
Paul Preuvost <paulantoinepreuvostpro@gmail.com>
2024-08-13 11:27:59 +0200

    GOOD PROVISION SCRIPT

--- provision/install_docker.sh
+++ provision/install_docker.sh
@@ -1,25 +1,22 @@
-# echo "START INSTALL DOCKER"
+echo "START INSTALL DOCKER"
 
-# sudo apt-get update -y
-# sudo apt-get install -y \
-#     ca-certificates \
-#     curl \
-#     gnupg \
-#     lsb-release
+sudo apt-get update -y
 
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-# sudo apt-get update -y
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "deb [arch=amd64] https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo systemctl start docker
-# sudo systemctl enable docker
+sudo apt-get update -y
 
-# docker --version
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# echo "DOCKER INSTALLATION COMPLETED"
+docker --version
+
+echo "DOCKER INSTALLATION COMPLETED"
